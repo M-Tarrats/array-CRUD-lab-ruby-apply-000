4c2df75dd89b29a8f01a285816191245e9a7a1ff
energetic-port-0799 <m.tarrats@yahoo.com>
2019-08-07 00:35:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array = ["I", "am", "realy", "learning"]
+  element = "wow"
+  array.pop(element)
 end
 
 def remove_element_from_end_of_array(array)
