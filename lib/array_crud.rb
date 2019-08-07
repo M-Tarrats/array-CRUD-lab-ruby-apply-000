0439e2e117ed107165ddb6a309ec25b912ddf127
energetic-port-0799 <m.tarrats@yahoo.com>
2019-08-07 00:55:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  puts ["0"]
+  puts array.inspect(0)
 end
 
 def retrieve_first_element_from_array(array)
