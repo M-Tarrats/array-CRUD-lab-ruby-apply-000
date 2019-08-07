d13bbccbfe6c8c0208e180b8d14288acd85e4ade
energetic-port-0799 <m.tarrats@yahoo.com>
2019-08-07 00:08:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["one", "two", "three", "four"].push("five")
+  puts ["one", "two", "three", "four"].push("five")
 end
 
 def add_element_to_start_of_array(array, element)
